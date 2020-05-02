982a4741955e4a0bdb9d698f1995e56b2935683a
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2020-05-03 00:07:03 +0200

    List regions & departements on home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,32 +2,70 @@ import React from "react"
 import { graphql } from 'gatsby';
 
 export default ({
-  data: { allSitePage: { groups } = {} } = {},
+  data: {
+    allRegion: { regions },
+    allCommune: { communes },
+  },
 }) => {
   return (
     <div>
-      <pre>
-        {groups.map(({ name, pages }) => (
-          <section key={name}>
-            <h2>{name}</h2>
-            <ul>
-              {pages.map(({ path }) => (
-                <li key={path}><a href={path}>{path}</a></li>
-              ))}
-            </ul>
-          </section>
+      <h2>Régions</h2>
+
+      {regions.map(({ nom, slug, departements }) => (
+        <section key={slug}>
+          <h3>{nom}</h3>
+
+          <div>
+            {departements.map(({ dptSlug, dptNom, code }, index) => (
+              <>
+                {Boolean(index) && ', '}
+                <span key={dptSlug}>
+                  <a href={`/${slug}/${dptSlug}`}>
+                    {dptNom}
+                  </a>
+                  {Boolean(code) && ` (${code})`}
+                </span>
+              </>
+            ))}
+          </div>
+        </section>
+      ))}
+
+      <h2>Communes hors régions</h2>
+
+      <div>
+        {communes.map(({ nom, slug }, index) => (
+          <>
+            {Boolean(index) && ', '}
+            <span key={slug}><a href={`/${slug}`}>{nom}</a></span>
+          </>
         ))}
-      </pre>
+      </div>
     </div>
   );
 }
 
 export const query = graphql`
-  query MyQuery {
-    allSitePage(sort: {fields: path}) {
-      groups: group(field: pluginCreator___name) {
-        name: fieldValue
-        pages: nodes { path }
+  query homeQuery {
+    allRegion(sort: {fields: slug}) {
+      regions: nodes {
+        nom
+        slug
+        departements: childrenDepartement {
+          code
+          dptNom: nom
+          dptSlug: slug
+        }
+      }
+    }
+
+    allCommune(
+      filter: {codeRegion: {eq: null}},
+      sort: {fields: slug}
+    ) {
+      communes: nodes {
+        nom
+        slug
       }
     }
   }
