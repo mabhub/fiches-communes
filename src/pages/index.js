4dc2098a4d0ea112d5142801c5b89c3358f52cbe
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2020-05-02 12:24:59 +0200

    List all available pages on home

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,34 @@
 import React from "react"
+import { graphql } from 'gatsby';
 
-export default () => <div>Hello world!</div>
+export default ({
+  data: { allSitePage: { groups } = {} } = {},
+}) => {
+  return (
+    <div>
+      <pre>
+        {groups.map(({ name, pages }) => (
+          <section key={name}>
+            <h2>{name}</h2>
+            <ul>
+              {pages.map(({ path }) => (
+                <li key={path}><a href={path}>{path}</a></li>
+              ))}
+            </ul>
+          </section>
+        ))}
+      </pre>
+    </div>
+  );
+}
+
+export const query = graphql`
+  query MyQuery {
+    allSitePage(sort: {fields: path}) {
+      groups: group(field: pluginCreator___name) {
+        name: fieldValue
+        pages: nodes { path }
+      }
+    }
+  }
+`;
